faeefb54c4e5f5bc67d97ed1a09b0ddf4fec3e10
kongfm <kongp3@outlook>
2019-03-18 18:47:47 +0800

    添加callback entity

---
+++ kernel/src/main/java/com/transfereasy/entity/Callback.java
@@ -0,0 +1,8 @@
+package main.java.com.transfereasy.entity;
+
+import lombok.Data;
+
+@Data
+public class Callback {
+    private String notify_code;
+}
---
+++ kernel/src/main/java/com/transfereasy/entity/Data.java
@@ -0,0 +1,4 @@
+package main.java.com.transfereasy.entity;
+
+public class Data {
+}
